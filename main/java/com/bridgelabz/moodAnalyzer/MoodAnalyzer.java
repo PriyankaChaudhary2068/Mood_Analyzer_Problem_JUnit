4d7dbdcc9259f8e41ef58f1fe82e46cffa450c9d
chutki12 <priyankachaudhary2068>
2022-08-01 02:58:08 +0530

    ADDED : This Code For To Handle Exception If User Provides Invalid Mood

--- main/java/com/bridgelabz/moodAnalyzer/MoodAnalyzer.java
+++ main/java/com/bridgelabz/moodAnalyzer/MoodAnalyzer.java
@@ -14,14 +14,17 @@ public class MoodAnalyzer {
 	    }
 
 	    public String analyzeMood() {
-	        if (message.contains("I am in sad mood"))
-	            return "SAD";
-	        else
+	        try {
+	            if (message.contains("I am in sad mood"))
+	                return "SAD";
+	            else
+	                return "HAPPY";
+	        } catch (NullPointerException e) {
 	            return "HAPPY";
-	        
+	            
+	        }
 	    }
-	}
-	   
+}
 	
 
 
--- test/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerTest.java
+++ test/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerTest.java
@@ -21,6 +21,14 @@ public class MoodAnalyzerTest {
 	        String mood = moodAnalyzer.analyzeMood();
 	        Assert.assertEquals("HAPPY",mood);
 	    }
+	    
+	    @Test
+	    public void givenMessage_WhenNull_ShouldReturnHappy(){
+	        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("null");
+	        String mood = moodAnalyzer.analyzeMood();
+	        Assert.assertEquals("HAPPY",mood);
+	    }
 	}
+	
 
 
