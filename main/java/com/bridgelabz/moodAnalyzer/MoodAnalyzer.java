7d72054a20ba3f04f25fac44f35125688f2eddf0
chutki12 <priyankachaudhary2068>
2022-08-01 02:21:44 +0530

    ADDED : Welcome To Mood Analyzer Problem

---
+++ main/java/com/bridgelabz/moodAnalyzer/MoodAnalyzer.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.moodAnalyzer;
+
+
+public class MoodAnalyzer {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Welcome To Mood Analyzer Problem");
+
+	}
+
+}
